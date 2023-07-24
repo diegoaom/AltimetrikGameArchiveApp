428cad969b82f2b963ab2ccbcc9097cfb4cbe78c
Diego Olivieri <diegoolivierivii@gmail.com>
2023-07-24 16:01:56 -0300

    feat: modal responsive, sidebar tablet and login validation styles added

--- js/login.js
+++ js/login.js
@@ -2,6 +2,8 @@ const JSON_SERVER_URL = "http://localhost:3000/login";
 const emailInput = document.querySelector("#email-input");
 const passwordInput = document.querySelector("#password-input");
 const loginBtn = document.querySelector("#login-btn");
+const visibilityBtn = document.querySelector("#password-visibility-btn");
+const eyeIcon = document.querySelector("#eye-icon");
 
 
 const login = async () => {
@@ -25,6 +27,22 @@ const login = async () => {
         console.log('user rejected');
     }
 };
+
+const togglePasswordVisibility = () => {
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeIcon.classList.add("eye-slashed");
+    } else {
+        passwordInput.type = "password";
+        eyeIcon.classList.remove("eye-slashed");
+    }
+
+}
+
+visibilityBtn.addEventListener("click", () => {
+    togglePasswordVisibility();
+})
 
 loginBtn.addEventListener("click", (e) => {
 
